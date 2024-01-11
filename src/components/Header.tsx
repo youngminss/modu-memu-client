5e8a9263421292ceaaf3649ea79576ca020f6502
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-01-11 23:09:28 +0900

    fix: change pixel to rem

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 // 카카오 로그인 버튼은 운영 배포 이후에 설정
 const Header = () => {
   return (
-    <nav className={`flex h-[78px] w-screen max-w-[480px] items-center`}>
-      <div className="ml-[20px]">
+    <nav className={`flex h-[7.8rem] w-screen max-w-[48rem] items-center`}>
+      <div className="ml-[2rem]">
         <Image src="/images/logo.svg" alt="logo" height={30} width={130} />
       </div>
     </nav>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex justify-center">
-      <div className="h-[100dvh] w-screen max-w-[480px] shadow-xl">{children}</div>
+      <div className="h-[100dvh] w-screen max-w-[48rem] shadow-xl">{children}</div>
     </div>
   )
 }
