2e3620499c1d2bf04e066bcd24a0f084f6f2380c
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-01-14 19:18:12 +0900

    style: add locate search ui component in first render page

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
+import SearchLocation from "@/src/components/SearchLocation"
+
 const HomePage = () => {
   return (
-    <>
-      <a href="https://www.figma.com/file/S0j3Up3jMhnPRAGdwIzmxm/Design?type=design&node-id=1307-25624&mode=design&t=ObNWtYE0TZxiQ4UP-0">
-        초기 화면 (클릭 시에 피그마 페이지로 이동)
-      </a>
-    </>
+    <div>
+      <SearchLocation />
+    </div>
   )
 }
 
