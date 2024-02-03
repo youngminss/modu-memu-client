851e89b514817c7db89c2842c435d8c273e8f159
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-02-03 17:57:08 +0900

    chore: create new route

---
+++ app/search/page.tsx
@@ -0,0 +1,5 @@
+import SearchPage from "@/src/pages/search/page"
+
+export default function Search() {
+  return <SearchPage />
+}
---
+++ src/pages/search/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+const SearchPage = () => {
+  return <div>새로운 페이지입니다</div>
+}
+
+export default SearchPage
