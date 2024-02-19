3e9ecdd492b7b0c81ec5488de8d3f75269ad4c25
위영민 <youngminieo1005@gmail.com>
2024-02-20 00:26:41 +0900

    chore: Small changed

--- src/components/InputPostCode.tsx
+++ src/components/InputPostCode.tsx
@@ -36,7 +36,7 @@ const InputPostCode = ({
 
       // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
       if (extraAddr !== "") {
-        extraAddr = " (" + extraAddr + ")"
+        extraAddr = "(" + extraAddr + ")"
       }
     }
 
